b2a19a3a1a109b563f7c93484d509f6a4e54ae3c
angela <angela@londonappbrewery.com>
2019-10-28 14:45:29 +0000

    Initial Commit

---
+++ ViewControllerLifecycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewControllerLifecycle
+//
+//  Created by Angela Yu on 28/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
